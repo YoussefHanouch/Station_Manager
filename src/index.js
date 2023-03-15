2a8d53d13eeb87c1717fda5123af184e4c658de8
YoussefHanouch <7anouch99@gmail.com>
2023-03-15 15:09:19 +0100

    premier test

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React  from 'react';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function App(){
+  
+
+
+  return (
+    <div>
+    <h1>hello</h1>
+    
+    </div>
+
+  )
+}
+ root.render(
+ 
+ 
+ <App/> 
+  
+ 
+  
+ 
+  
+ )
+
+
