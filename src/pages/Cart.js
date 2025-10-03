d634fb17ad4b61b7cf94c20655011d929914635d
YoussefHanouch <7anouch99@gmail.com>
2025-10-03 04:19:54 +0100

    Vos modifications

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from './context/CartContext';
-
+ import './index.css'
 const Cart = () => {
   const { 
     cart, 
